b7693f60eabfe6cb505e81bb9e50f4f45401b026
hiaximize <dbarkerit@gmail.com>
2019-09-05 19:08:55 -0400

    changed the configurations of files

---
+++ app/javascript/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class App extends React.Component{
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return(
+            <h2>hola</h2>
+        )
+    }
+}
+export default App
