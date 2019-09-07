ead87618af2c7dce7bd56b7447a58e7616c98920
hiaximize <dbarkerit@gmail.com>
2019-09-07 02:16:54 -0400

    added some comment notes

--- app/models/gainz.rb
+++ app/models/gainz.rb
@@ -1,6 +1,9 @@
 class Gainz
+
+    # Our unique data base url
      DATABASE_URL = 'postgres://kcmranatsnkrqx:2d51d471383160ce3ae3ec1f11e1f3add50a287c4f9384abb92841193b39e9be@ec2-54-243-47-196.compute-1.amazonaws.com:5432/daqc908ate9rgm'
     
+    #  Code to handle either the cloud database or local DB connection
     if(ENV['DATABASE_URL'])
         uri = URI.parse(ENV['DATABASE_URL'])
         DB = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
@@ -8,6 +11,8 @@ class Gainz
         DB = PG.connect(host: "localhost", port: 5432, dbname: 'groupproject_development')
     end
 
+
+    # Index route
     def self.all
         results = DB.exec("SELECT * FROM exercise;")
         return results.map do | result |
@@ -21,6 +26,7 @@ class Gainz
         end
     end
 
+    # Show route
     def self.find(id)
         result = DB.exec("SELECT * FROM exercise WHERE exercise_id=#{id};")
         return {
